472ca6f42b635ce4f3e22f948c76d2916a76517e
David Pickett <pickettd@gmail.com>
2014-06-10 17:37:41 -1000

    Corrected input target number (from a test value). And renamed counter variable for clarity.

--- problem3.js
+++ problem3.js
@@ -13,27 +13,27 @@ What is the largest prime factor of the number 600851475143 ?
 
 
 // The target number from the problem statement.
-var target_number = 34;
+var target_number = 600851475143;
 
 // Declare the first loop variable and initialize the success boolean variable.
-var counter;
+var outer_counter;
 var success = false;
 
 // In this loop we look for factors of the target number. The biggest possible prime factor
 // for the target number is target_number / 2 so we will start the counter there.
-for (counter = Math.floor(target_number / 2); counter >= 2; counter -= 1) {
+for (outer_counter = Math.floor(target_number / 2); outer_counter >= 2; outer_counter -= 1) {
     // Check if the counter is a factor of the target number.
-    if (target_number % counter === 0) {
+    if (target_number % outer_counter === 0) {
         // Declare the second loop variable and initialize the success boolean variable.
         var isPrime = true;
         var inner_counter;
         
         // We know that counter is a factor of the target number, now we need to know if it is prime.
         // The inner_counter starts at 2 (all numbers are divisible by 1) and increments until 
-        // the inner_counter is greater than the square root of counter. As soon as a factor is
-        // found for counter we will break the inner loop.
-        for (inner_counter = 2; inner_counter * inner_counter <= counter; inner_counter += 1) {
-            if (counter % inner_counter === 0) {
+        // the inner_counter is greater than the square root of the outer counter. As soon as a factor is
+        // found for the outer counter we will break the inner loop.
+        for (inner_counter = 2; inner_counter * inner_counter <= outer_counter; inner_counter += 1) {
+            if (outer_counter % inner_counter === 0) {
                 isPrime = false;
                 break;
             }
@@ -42,7 +42,7 @@ for (counter = Math.floor(target_number / 2); counter >= 2; counter -= 1) {
         // If the previous loop checked all numbers from 2 until counter's square root
         // and didn't find a factor then we have found the solution.
         if (isPrime) {
-            console.log("Largest prime factor is: " + counter);
+            console.log("Largest prime factor is: " + outer_counter);
             success = true;
             break;
         }
